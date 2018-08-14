3196900ff313f9acbfadb5e0c0599bdf76df560c
aalfonso <alejandro.alfonso.albero@cern.ch>
2018-08-14 13:19:28 +0200

    Test main para leer alis

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 //Dummy main, para testear compilacion
+#include <string>
+#include <iostream>
+#include "alineacion.h"
+#include "equipo.h"
+#include "position.h"
+#include "Simu.h"
+using namespace std;
 
 int main(void)
 {
+  string nombre;
+  cin >> nombre;
+  nombre = "../test/"+nombre+"sht.txt";
+  alineacion ali(nombre);
+  equipo eq(nombre);
   return 0;
 }
