7f72113dc54cac523edcc375df06348e79fc149d
aalfonso <alejandro.alfonso.albero@cern.ch>
2018-08-14 17:03:05 +0200

    Mas cosas para testear en main

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,21 @@ using namespace std;
 
 int main(void)
 {
-  string nombre;
-  cin >> nombre;
-  nombre = "../test/"+nombre+"sht.txt";
-  alineacion ali(nombre);
+  string nombre, nombre2;
+  cout << "Equipo local: ";
+  //cin >> nombre;
+  cout << endl; nombre = "ros";
+  cout << "Equipo visitante: ";
+  //cin >> nombre2;
+  cout << endl; nombre2 = "ros";
+  alineacion ali(nombre+"sht.txt");
+  alineacion ali2(nombre2+"sht.txt");
   equipo eq(nombre);
+  equipo eq2(nombre2);
+  eq.Load();
+  eq2.Load();
+  ali.Link(&eq);
+  ali2.Link(&eq2);
+  cout << "Test correcto" << endl;
   return 0;
 }
