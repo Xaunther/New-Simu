ec1f0d852be57ffc9f894bdd03afc7e872cebb8d
Alejandro Alfonso <alex.1993.24@gmail.com>
2019-10-03 16:58:20 +0200

    Probar de simular un partido en travis

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,5 +2,8 @@
 set -ev
 #Only build if not coming from a pull request
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  #Compile
   cd build && cmake .. && make
+  #Test
+  cp New-simu ../test/ && mv ../test && ./New-simu
 fi
